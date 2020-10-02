b9d5672879adcf280446c8ef25d048ed01628d82
Illiya Chernyak <Illiya.Chernyak@outlook.com>
2020-10-02 13:20:05 +0300

    Create & first push

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const hbs = require("hbs");
+
+let app = express();
+app.set('view engine', 'hbs');
+
+app.get('/', (req, res) =>{
+    res.send("Hello, JS");
+});
+
+app.get('/weather', (req,res) => {
+    const weather = {
+        description: "Clear sky"
+    }
+    res.render('weather.hbs', {weather})
+});
+app.get('/weather/:city', (req,res) => {
+    res.send(req.params.city);
+});
+
+app.get('/login', (req,res) => {
+    res.send("login page");
+});
+
+app.listen(3000, () =>{
+    console.log("Example app listening onport 3000");
+});
