875ff32ec2f9b220878c87b7fc30eb55f18a2e1a
Illiya Chernyak <Illiya.Chernyak@outlook.com>
2020-10-02 14:32:37 +0300

    opnweather

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const fetch = require('node-fetch')
 const hbs = require("hbs");
 hbs.registerPartials(__dirname + '/views/partials');
 let app = express();
@@ -14,12 +15,19 @@ app.get('/weather', (req,res) => {
     }
     res.render('weather.hbs', {weather})
 });
-app.get('/weather/:city', (req,res) => {
-    res.send(req.params.city);
+app.get('/weather/:city', async function (req,res) {
+    var city = req.params.city;
+    var appId = '9f14818550a75877c7dcdd3994f0f5d6';
+    var url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${appId}`;
+    var result = await fetch(url);
+    var weather = await result.json();
+
+    res.render('weather.hbs',{city, weather});
 });
 app.get('/login', (req,res) => {
     res.send("login page");
 });
+
 
 
 
